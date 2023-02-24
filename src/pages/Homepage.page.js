6788b4adf5f53bb5c3792f247b75be054285cac0
shellyalmo <shellyalmo@gmail.com>
2023-02-24 10:46:59 +0200

    feat: homepage style and jsx done

--- src/pages/Homepage.page.js
+++ src/pages/Homepage.page.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar.component";
+import storeLogo from "../assets/shoes-logo.jpg";
+import "../styles/homepage.style.css";
 
 const Homepage = () => {
   return (
     <div>
       <Navbar />
+      <div className="background-container">
+        <img className="background-img" src={storeLogo} alt="" />
+        <h3>Welcome to the Shoes Store Manager.</h3>{" "}
+        <p>
+          {" "}
+          The online platform for store managers to view their shoes inventory,
+          edit shoes information and add new shoes.
+        </p>
+      </div>
     </div>
   );
 };
